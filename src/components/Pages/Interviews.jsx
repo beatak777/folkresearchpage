94ea6f4c39258988ada927e5147197a5d236440c
Beata Konya <beatakonya@gmail.com>
2024-06-03 13:19:51 +0100

    added video links

--- src/components/Pages/Interviews.jsx
+++ src/components/Pages/Interviews.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
 function Interviews() {
+  // Helper function to convert Google Drive links to embeddable URLs
+  const getEmbeddableUrl = (originalUrl) => {
+    // Extract the file ID from the Google Drive link
+    const fileId = originalUrl.match(/\/file\/d\/(.+?)\/(?:view|edit)?/)?.[1];
+    if (fileId) {
+      // Construct the embeddable URL
+      return `https://drive.google.com/file/d/${fileId}/preview`;
+    } else {
+      return null; // Invalid or unsupported URL format
+    }
+  };
+
   return (
     <div className="container">
       <h1>Interviews</h1>
@@ -11,10 +23,10 @@ function Interviews() {
           title="First Interview"
           width="560"
           height="315"
-          src="https://drive.google.com/file/d/1kDb4E55wtmGrF-hbUrfm3eyU5_qxeTp9/view?usp=drive_link"
-          frameborder="0"
+          src={getEmbeddableUrl("https://drive.google.com/file/d/1kDb4E55wtmGrF-hbUrfm3eyU5_qxeTp9/view?usp=drive_link")}
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
 
         {/* Second Interview */}
@@ -22,10 +34,10 @@ function Interviews() {
           title="Second Interview"
           width="560"
           height="315"
-          src="https://drive.google.com/file/d/VIDEO_ID_2/preview"
-          frameborder="0"
+          src={getEmbeddableUrl("https://drive.google.com/file/d/1D3F3J84e8vHgyM97dPaMIVPa0oYUQXRJ/view?usp=drive_link")}
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
 
         {/* Third Interview */}
@@ -33,10 +45,10 @@ function Interviews() {
           title="Third Interview"
           width="560"
           height="315"
-          src="https://drive.google.com/file/d/VIDEO_ID_3/preview"
-          frameborder="0"
+          src={getEmbeddableUrl("https://drive.google.com/file/d/1qYVxVNy_IWv-asCgUhqoGuoyPPsKsqWt/view?usp=sharing")}
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
 
         {/* Fourth Interview */}
@@ -44,10 +56,10 @@ function Interviews() {
           title="Fourth Interview"
           width="560"
           height="315"
-          src="https://drive.google.com/file/d/VIDEO_ID_4/preview"
-          frameborder="0"
+          src={getEmbeddableUrl("https://drive.google.com/file/d/1hApLCCN0AunR8y7H1UhJ6dLrKSsozHJ8/view?usp=sharing")}
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
@@ -55,3 +67,4 @@ function Interviews() {
 }
 
 export default Interviews;
+
